f28f7e3a9cb3b8adc71cc6eb1819e4c7a2dbf290
RedHammer221 <simeon.g.vasilev@gmail.com>
2024-06-29 20:06:30 +0200

    Extract method lowercaseTags

--- src/main/java/de/oncoding/webshop/repository/ProductRepository.java
+++ src/main/java/de/oncoding/webshop/repository/ProductRepository.java
@@ -7,30 +7,30 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ProductRepository {
+    List<ProductResponse> products = Arrays.asList(
+            new ProductResponse(
+                    "1",
+                    "RTX 4060",
+                    "asasdaftfufsf",
+                    555550,
+                    Arrays.asList("NVidia".toLowerCase(), "GPU".toLowerCase())
+            ),
+            new ProductResponse(
+                    "2",
+                    "RIntel Core I5 13600",
+                    "asasdaftfufsf",
+                    39080,
+                    Arrays.asList("Intel".toLowerCase(), "CPU".toLowerCase())
+            ),
+            new ProductResponse(
+                    "3",
+                    "AMD Ryzen 6",
+                    "asasdaftfufsf",
+                    34590,
+                    Arrays.asList("AMD".toLowerCase(), "CPU".toLowerCase())
+            )
+    );
     public List<ProductResponse>findAll(String tag){
-        List<ProductResponse> products = Arrays.asList(
-                new ProductResponse(
-                        "1",
-                        "RTX 4060",
-                        "asasdaftfufsf",
-                        555550,
-                        Arrays.asList("NVidia".toLowerCase(), "GPU".toLowerCase())
-                ),
-                new ProductResponse(
-                        "2",
-                        "RIntel Core I5 13600",
-                        "asasdaftfufsf",
-                        39080,
-                        Arrays.asList("Intel".toLowerCase(), "CPU".toLowerCase())
-                ),
-                new ProductResponse(
-                        "3",
-                        "AMD Ryzen 6",
-                        "asasdaftfufsf",
-                        34590,
-                        Arrays.asList("AMD".toLowerCase(), "CPU".toLowerCase())
-                )
-        );
 
         if (tag == null)
             return products;
@@ -39,15 +39,21 @@ public class ProductRepository {
             List<ProductResponse> filtered = new ArrayList<>();
 
             for (ProductResponse p:products) {
-                List<String> lowercaseTags = new ArrayList<>();
-                for (String t:p.getTags()) {
-                    lowercaseTags.add(t.toLowerCase());
-                }
+                List<String> lowercaseTags = lowercaseTags(p);
                 if(lowercaseTags.contains(tag))
                     filtered.add(p);
             }
             return filtered;
         }
     }
+
+    private List<String> lowercaseTags(ProductResponse p) {
+        List<String> lowercaseTags = new ArrayList<>();
+        for (String t: p.getTags()) {
+            lowercaseTags.add(t.toLowerCase());
+        }
+        return lowercaseTags;
+    }
+
 }
 
