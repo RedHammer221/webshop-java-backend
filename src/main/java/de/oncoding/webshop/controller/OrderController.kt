0692a3655f67615d9bfc1d5706d7223af3adf685
RedHammer221 <simeon.g.vasilev@gmail.com>
2024-08-24 12:06:29 +0200

    Introduce dependency injection

--- src/main/java/de/oncoding/webshop/controller/OrderController.kt
+++ src/main/java/de/oncoding/webshop/controller/OrderController.kt
@@ -1,11 +1,13 @@
 package de.oncoding.webshop.controller
 
+import de.oncoding.webshop.exceptions.WebshopException
 import de.oncoding.webshop.model.OrderCreateRequest
 import de.oncoding.webshop.model.OrderPositionCreateRequest
 import de.oncoding.webshop.model.OrderResponse
 import de.oncoding.webshop.repository.OrderRepository
 import de.oncoding.webshop.repository.ProductRepository
 import de.oncoding.webshop.service.OrderService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -20,7 +22,7 @@ class OrderController(
     fun createOrder(
         @RequestBody request: OrderCreateRequest
     ): OrderResponse {
-        return orderService.createOrder(request)
+            return orderService.createOrder(request)
     }
 
     @PostMapping("/orders/{id}/positions")
---
+++ src/main/java/de/oncoding/webshop/exceptions/WebshopException.kt
@@ -0,0 +1,14 @@
+package de.oncoding.webshop.exceptions
+
+import org.springframework.http.HttpStatus
+import java.lang.Exception
+
+class WebshopException(
+    override val message: String,
+    val statusCode: HttpStatus
+): RuntimeException(message)
+
+class IdNotFoundException(
+    override val message: String,
+    val statusCode: HttpStatus
+): RuntimeException(message)
